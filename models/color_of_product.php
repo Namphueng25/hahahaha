f8aedcf0246ed134af9d14b20ba45144b029a39c
Namphueng25 <namphueng.k@ku.th>
2021-09-30 22:38:44 +0700

    Update color_of_product.php

--- models/color_of_product.php
+++ models/color_of_product.php
@@ -1,24 +1,24 @@
 <?php class color_of_product{
-    public $id_cop,$id_product,$id_color;
-    public function __construct($id_cop,$id_product,$id_color)
+    public $cp_id,$cp_code,$cp_color;
+    public function __construct($cp_id,$cp_code,$cp_color)
     {
-        $this->id_cop = $id_cop;
-        $this->id_product = $id_product;
-        $this->id_color = $id_color;
+        $this->cp_id = $cp_id;
+        $this->cp_code = $cp_code;
+        $this->cp_color = $cp_color;
         
     }
     public static function getAll()
     {
         $color_of_productList =[];
         require("connect_database.php");
-        $sql = "select * from color_of_product";
+        $sql = "select * from ColorProduct";
         $result = $conn->query($sql);
         while($my_row = $result->fetch_assoc())
         {
-            $id_cop = $my_row[id_cop];
-            $id_product = $my_row[id_product];
-            $id_color = $my_row[id_color];
-            $color_of_productList[] = new color_of_product($id_cop,$id_product,$id_color);
+            $cp_id = $my_row[cp_id];
+            $cp_code = $my_row[cp_code];
+            $cp_color = $my_row[cp_color];
+            $color_of_productList[] = new color_of_product($cp_id,$cp_code,$cp_color);
         }
         require("connection_close.php");
         return $color_of_productList;
