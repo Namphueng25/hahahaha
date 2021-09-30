2f2e6d061df3e8cc7036461b5cad4c7f651588b5
Namphueng25 <namphueng.k@ku.th>
2021-09-30 22:38:54 +0700

    Update order.php

--- models/order.php
+++ models/order.php
@@ -1,46 +1,43 @@
 <?php class Order{
-     public $id_order_cus;
-     public $date_order;
-     public $id_staff;
-     public $id_customer;
-    public $fname_staff;
-    public $name_customer;
+     public $id_order;
+     public $date;
+     public $condition;
+     public $id_cus;
+     public $Staff_id;
+     public $เงื่อนไข;
 
-    public function __construct($id_order_cus,$date_order,$fname_staff,$name_customer)
+
+    public function __construct($id_order,$date,$condition,$id_cus,$Staff_id,$เงื่อนไข)
     {
-        $this->id_order_cus = $id_order_cus;
-        $this->date_order = $date_order;
-        $this->fname_staff = $fname_staff;
-        $this->name_customer = $name_customer;
+        $this->id_order = $id_order;
+        $this->date = $date;
+        $this->condition = $condition;
+        $this->id_cus = $id_cus;
+        $this->Staff_id = $Staff_id;
+        $this->เงื่อนไข = $เงื่อนไข;
     }
     public static function getAll()
     {
         $orderList=[];
         require("connect_database.php");
-        $sql="SELECT * FROM order_cutomer NATURAL JOIN staff NATURAL JOIN customer";
+        $sql="SELECT * FROM customer_order ";
         $result=$conn->query($sql);
         while($my_row=$result->fetch_assoc())
         {
-            $id_order_cus = $my_row[id_order_cus];
-            $date_order = $my_row[date_order];
-            $fname_staff= $my_row[fname_staff];
-            $name_customer = $my_row[name_customer];
+            $id_order = $my_row[id_order];
+            $date = $my_row[date];
+            $condition = $my_row[condition ];
+            $id_cus = $my_row[id_cus];
+            $Staff_id = $my_row[Staff_id];
+            $เงื่อนไข = $my_row[เงื่อนไข];
             
-            $orderList[] = new Order($id_order_cus,$date_order,$fname_staff,$name_customer);
+            $orderList[] = new Order($id_order,$date,$condition,$id_cus,$Staff_id,$เงื่อนไข);
         }
         require("connection_close.php");
         return $orderList;
 
     }
-     public static function Add($id_order_cus,$date_order,$fname_staff,$name_customer)
-     { 
-        require("connect_database.php");
-        $sql = "insert into Order(id_order_cus,date_order,fname_staff,name_customer)
-        values('$id_order_cus','$date_order','$fname_staff','$name_customer')";
-        $result = $conn->query($sql);
-        require("connection_close.php");
-        return  "add success $reult rows";
-     }
+
 
 
 
