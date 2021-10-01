ac73f6105eb68b6e1a0100464c8554e00b908d64
Namphueng25 <namphueng.k@ku.th>
2021-10-01 15:11:21 +0700

    Update order_controller.php

--- controllers/order_controller.php
+++ controllers/order_controller.php
@@ -4,11 +4,7 @@
         $order_List=Order::getAll();
         require_once("./views/order/index_order.php");
     }
-    public function newOrder(){
-        $staff_List=staff::getAll();
-        $customer_List=customer::getAll();
-        require_once('./views/order/newOrder.php');
-    }
+
 
 
 
