f322337adf9f05556c8a4c1b18566237580fbd43
Namphueng25 <91516817+Namphueng25@users.noreply.github.com>
2021-10-05 02:00:31 +0700

    Delete controllers directory

--- controllers/range_price_controller.php
+++
@@ -1,9 +0,0 @@
-<?php class Range_PriceController{
-    public function index()
-    {
-        $range_price_List=range_price::getAll();
-        require_once("./views/range_price/index_range_price.php");
-    }
-
-}
-?>
