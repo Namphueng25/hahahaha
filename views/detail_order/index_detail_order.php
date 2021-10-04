317400c448fbc2749ecec6b7f40f7a2f22eeb453
Namphueng25 <91516817+Namphueng25@users.noreply.github.com>
2021-10-05 02:00:53 +0700

    Delete views directory

--- views/detail_order/index_detail_order.php
+++
@@ -1,9 +0,0 @@
-<table border = 1>
-new Detail_order <a href=?controller=student&action=newdetail_order> click</a><br>
-<tr> <td>id_doc</td> <td>print_color</td> <td>amount_order_cus</td> <td>id_order_cus</td> <td>id_cop</td> <td>update</td> <td>delete</td> </tr> 
-<?php foreach($detail_order_List as $detail_order)
-{
-    echo "<tr> <td>$detail_order->id_doc</td> <td>$detail_order->print_color</td> <td>$detail_order->amount_order_cus</td> <td>$detail_order->id_order_cus</td> <td>$detail_order->id_cop</td> <td>update</td> <td>delete</td> </tr>";
-}
-echo "</table>";
-?>
